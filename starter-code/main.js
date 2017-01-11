e3bc640805549605dc134de1fc035ad1f6713afa
KEVIN KELLER <keller_kevin@bah.com>
2017-01-10 19:08:49 -0500

    createCards function made

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var	cardFour = "king";
 
-if (cardOne === cardTwo || cardThree === cardFour) {
+/*if (cardOne === cardTwo || cardThree === cardFour) {
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again");
+}*/
+
+var createCards = function() {
+	var game = document.getElementById('game-board');
+
+	for (var i=0; i<4; i++) {
+		var newDiv = document.createElement('div');
+		newDiv.id = 'game-board';
+		newDiv.className = 'card';
+		game.appendChild(newDiv);
+	};
 }
+
+createCards();
